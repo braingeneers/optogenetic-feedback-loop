41e40c45d3739429557872b718e33e5a26b7bb1e
Raspberry PiKate <kate.voitiuk@gmail.com>
2018-10-10 12:32:07 -0700

    Optimized array code written, bug when entering 1 -- software or physical wiring issue?

--- Makefile
+++ Makefile
@@ -8,12 +8,13 @@ BLINK_SRC = ledarray/src/blink.c #src/crack.h src/main.cc src/server.cc src/serv
 74HC595_LED_SRC = 74HC595_LED.c
 UI_74_SRC = ui74.c
 SR_SRC = sr.c
+OPTI_UI_74_SRC = optimized_ui74.c
 
-all: blink 74HC595_LED sr ui74HC595
+all: blink 74HC595_LED sr ui74HC595 opti_ui74HC595
 
 blink:	$(BLINK_SRC)
 	$(CC) -o $@ $^ $(CFLAGS)
-	
+
 #test:  $(TEST_SRC) crack.o
 #	$(CC) -o $@ $^ $(CFLAGS)
 
@@ -27,6 +28,11 @@ ui74HC595:  $(UI_74_SRC)
 	$(CC) -o $@ $^ $(CFLAGS) 
 
 
+opti_ui74HC595:  $(OPTI_UI_74_SRC)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+
+
 clean:
-	@rm blink 74HC595_LED sr ui74HC595
+	@rm blink 74HC595_LED sr ui74HC595 opti_ui74HC595
 
---
+++ optimized_ui74.c
@@ -0,0 +1,107 @@
+#include <bcm2835.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#define   SDI   16 //27   //serial data input
+#define   RCLK  20 //28   //memory clock input(STCP)
+#define   SRCLK 21 //29   //shift register clock input(SHCP)
+
+//unsigned char LED[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+#define LEDS 24 //leds per organoid
+#define LED_MASK 0x800000
+
+
+void pulse(int pin){
+	bcm2835_gpio_write(pin, HIGH);
+	bcm2835_gpio_write(pin, LOW);
+	//digitalWrite(pin, 0);
+	//digitalWrite(pin, 1);
+}
+
+void SIPO(unsigned int byte){
+	int i;
+	int out;
+
+	for(i=0;i<LEDS;i++){
+		//out = 1;
+		out = ((byte & (LED_MASK >> i)) > 0);
+		printf("out: %d, %x\n", out, out);
+		bcm2835_gpio_write(SDI, out);
+		//digitalWrite(SDI, out);
+		pulse(SRCLK);
+	}
+
+}
+
+void init(void){
+	bcm2835_gpio_fsel(SDI, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(RCLK, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(SRCLK, BCM2835_GPIO_FSEL_OUTP);
+
+	bcm2835_gpio_write(SDI, LOW);
+	bcm2835_gpio_write(RCLK, LOW);
+	bcm2835_gpio_write(SRCLK, LOW);
+
+	//pinMode(SDI, OUTPUT); //make P0 output
+	//pinMode(RCLK, OUTPUT); //make P0 output
+	//pinMode(SRCLK, OUTPUT); //make P0 output
+
+	//digitalWrite(SDI, 0);
+	//digitalWrite(RCLK, 0);
+	//digitalWrite(SRCLK, 0);
+
+        SIPO(0);
+        pulse(RCLK);
+        delay(500);
+
+        SIPO(0);
+        pulse(RCLK);
+        delay(500);
+
+
+}
+
+
+int main(void) {
+
+	if(!bcm2835_init()){
+		cout << "Init failed" << endl;
+	 	return 1;
+	}
+
+
+	//if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
+	//	printf("setup wiringPi failed !");
+	//	return 1; 
+	//}
+
+	//init();
+
+	while(1){
+
+		int pattern;
+//		pattern = rand() & rand();
+		cout <<  "What array configuration?" << endl;
+		cin >> pattern;
+		cout << endl;
+
+		//if(pattern == -1) break;
+		SIPO(pattern);
+		pulse(RCLK);
+		delay(25);
+
+//		SIPO(0);
+//		pulse(RCLK);
+//		delay(50);
+		//digitalWrite(RCLK, 0);
+
+
+	}
+
+	bcm2835_close();
+
+	return 0;
+}
