a73aff848de097c239512a5e3b61009a15a08d44
Raspberry PiKate <kate.voitiuk@gmail.com>
2018-10-09 18:56:48 -0700

    74HC595 starting code

--- 74HC595_LED.c
+++ 74HC595_LED.c
@@ -1,24 +1,27 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define   SDI   0   //serial data input
-#define   RCLK  1   //memory clock input(STCP)
-#define   SRCLK 2   //shift register clock input(SHCP)
+#define   SDI   27   //serial data input
+#define   RCLK  28   //memory clock input(STCP)
+#define   SRCLK 29   //shift register clock input(SHCP)
 
 unsigned char LED[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 
 
-void pulse(int pin)
-{
+void pulse(int pin){
 	digitalWrite(pin, 0);
+	//delay(50);
 	digitalWrite(pin, 1);
 }
 
-void SIPO(unsigned char byte)
-{
+void SIPO(unsigned char byte){
 	int i;
+	int out;
 
-	for(i=0;i> i)) > 0));
+	for(i=0;i<8;i++){
+		out = ((byte & (0x80 >> i)) > 0);
+		printf("out: %d, %x\n", out, out);
+		digitalWrite(SDI, out);
 		pulse(SRCLK);
 	}
 }
@@ -44,15 +47,24 @@ int main(void)
 	}
 
 	init();
+	printf("Initialized!\n");
 
 	while(1){
+
+  		printf("First!\n");
+
+
 		for(i=0;i<8;i++){
 			SIPO(LED[i]);
 			pulse(RCLK);
 			delay(150);
-			//printf("i = %d\n",i);
+			printf("i = %d\n",i);
 		}
+
 		delay(500);
+
+  printf("Second!\n");
+
 
 		for(i=0;i<3;i++){
 			SIPO(0xff);
@@ -65,12 +77,19 @@ int main(void)
 		delay(500);
 //		digitalWrite(RCLK,0);
 
+  printf("Third!\n");
+
+
 		for(i=0;i<8;i++){
 			SIPO(LED[8-i-1]);
 			pulse(RCLK);
 			delay(150);
 		}
 		delay(500);
+
+
+  printf("Fourth!\n");
+
 
 	    for(i=0;i<3;i++){
 			SIPO(0xff);
--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 CFLAGS = -Wall -Isrc -lbcm2835 -g
 CC = g++ -std=c++17
+CCN = gcc 
+CNFLAGS = -Wall -lwiringPi
 
 BLINK_SRC = ledarray/src/blink.c #src/crack.h src/main.cc src/server.cc src/server.h
 #TEST_SRC = src/crack.h src/test.cc src/server.cc src/server.h
+74HC595_LED_SRC = 74HC595_LED.c
 
-all: blink
+all: blink 74HC595_LED
 
 blink: $(BLINK_SRC)
 	$(CC) -o $@ $^ $(CFLAGS)
@@ -12,7 +15,9 @@ blink: $(BLINK_SRC)
 #test:  $(TEST_SRC) crack.o
 #	$(CC) -o $@ $^ $(CFLAGS)
 
+74HC595_LED:  $(74HC595_LED_SRC)
+	$(CCN) -o $@ $^ $(CNFLAGS) 
 
 clean:
-	@rm blink
+	@rm blink 74HC595_LED
 
