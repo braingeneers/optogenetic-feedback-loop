dca960d89aebfe432ea22a0f689312c8e855fc27
Raspberry PiKate <kate.voitiuk@gmail.com>
2018-11-12 23:58:05 -0800

    Python role switch, master is client of organoid

---
+++ src/organoid/client.py
@@ -0,0 +1,31 @@
+import sys
+import socket
+#import numpy as np
+import random
+import time
+
+
+def main():
+
+		print 'Arg1:', sys.argv[1], ' Arg2:', sys.argv[2]
+		s = socket.socket() #create socket
+		port = int(sys.argv[2]) #bind to port
+
+		# connect to server
+		host_ip = socket.gethostbyname(str(sys.argv[1]))
+		s.connect((host_ip, port))
+
+		while True:
+			# send 8 bit number
+			s.send(str(random.randint(0,255)))
+			out = s.recv(128) # receive echo from client
+			print 'Recieved:', out
+			time.sleep(1)
+
+		#-----------------------------------------------
+		s.close() #close socket
+		#-----------------------------------------------
+
+
+if __name__ == '__main__':
+	  main()
--- src/organoid/organoid.py
+++ src/organoid/organoid.py
@@ -8,30 +8,40 @@ from PIL import Image
 import socket
 
 
+
 def main():
 
-		print 'Arg1:', sys.argv[1], '  Arg2:', sys.argv[2]
+		print 'Arg1:', sys.argv[1]
 		s = socket.socket() #create socket
-		port = int(sys.argv[2]) #bind to port
 
-		# connect to server
-		host_ip = socket.gethostbyname(str(sys.argv[1]))
-		s.connect((host_ip, port))
+		port = int(sys.argv[1])
+		s.bind(('', port)) #bind to port
+		print "socket binded to %s" %(port)
 
 		seq_num = 0 #feedback loop sequence number
 
+		# listen on socket
+		s.listen(5)
 		while True:
-			# receive data from server
-			input = s.recv(1024)
-			print 'Master sent', input, int(input)
-			#-----------------------------------------------
-			s.send(input)
 
-			#Organoid Simulation
-			organoid(seq_num, int(input))
+		# Establish connection with client
+			c, addr = s.accept()
+			print 'Got connection from', addr
 
-			seq_num+=1 #increment sequence number
+			while True:
+					# receive 8 bit number from client
+					input = c.recv(128)
+					print 'Master sent', input, int(input)
+					# echo 8 bit number
+					c.send(input)
 
+					#Organoid Simulation
+					organoid(seq_num, int(input))
+
+					seq_num+=1 #increment sequence number
+
+			# Close connection with client
+			c.close()
 		#-----------------------------------------------
 		s.close() #close socket
 		#-----------------------------------------------
@@ -47,5 +57,5 @@ def organoid(seq_num, input_pattern):
 	return;
 
 
-if __name__=="__main__":
-   main()
+if __name__ == '__main__':
+	  main()
